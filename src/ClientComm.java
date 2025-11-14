1d248f1dbc79894e1de0891fea7e0421a3412683
LisaLisaBz <aarioli@unibz.it>
2025-11-14 11:50:24 +0100

    classi base

--- src/ClientComm.java
+++ src/ClientComm.java
@@ -1,3 +1,6 @@
 public ClientComm implements ClientCommInterface {
+
+    @Override
+    void publishProblem(byte[] hash, int problemsize) throws Exception;
     
 }
--- src/ServerComm.java
+++ src/ServerComm.java
@@ -1,3 +1,9 @@
 public ServerComm implements ServerCommInterface {
+
+    @Override
+    public void register (String teamName, ClientCommInterface cc) throws Exception;
+
+    @Override
+    public void submitSolution(String name, String sol) throws Exception;
     
 }
